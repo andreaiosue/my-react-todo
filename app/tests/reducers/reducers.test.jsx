005682a63607d6cba73aeb6758ef2b68c3a86d32
andrea-iosue <andrea.iosue@gmail.com>
2017-06-18 19:21:35 +0200

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,39 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var state = [{
+        id: 1,
+        text: 'hello',
+        completed: false,
+        createdAt: '01/01/2000',
+        completedAt: undefined
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      var res = reducers.todosReducer(df(state), df(action));
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toExist();
+
+      var res2 = reducers.todosReducer(df(res), df(action));
+      expect(res2[0].completed).toEqual(false);
+      expect(res2[0].completedAt).toNotExist();
+    });
+  });
 });
